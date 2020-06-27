792743d3bd08cd7625a6169cdf685edb45f5740a
tanwaikin <45248248+tanwaikin@users.noreply.github.com>
2020-06-27 17:51:08 +0800

    蚂蚁搬家

--- 10_classDestructor.cpp
+++ 10_classDestructor.cpp
@@ -1,5 +1,34 @@
 //
 // Created by brother_b on 2020/6/27.
 //
-
+//析构函数
 //todo 不懂
+#include "iostream"
+
+using namespace std;
+
+class ClassA {
+public:
+    ClassA();
+
+    ~ClassA();
+
+    void print();
+};
+
+ClassA::ClassA() {
+    cout << "constructing" << endl;
+}
+
+ClassA::~ClassA() { //析构函数 删除对象时执行的函数
+    cout << "destructing" << endl;
+}
+
+void ClassA::print() {
+    cout << "hello" << endl;
+}
+
+int main() {
+    ClassA a;
+    a.print();
+}
